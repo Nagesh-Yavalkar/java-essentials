27f431db6f71ee243d20baab80ee10aebf9abe60
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-09 13:40:33 +0530

    Real time example of remove in company

---
+++ Collection/Iteratoreg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Iteratoreg{
+    public static void main(String[] args) {
+        ArrayList<String> it = new ArrayList<>();
+        it.add("coupon1");
+        it.add("coupon2");
+        it.add("coupon3");
+        it.add("coupon4");
+        it.add("coupon end");
+        it.add("coupon5");
+        System.out.println(it);
+
+        Iterator<String>i = it.iterator();
+        while (i.hasNext()) {
+            String b = i.next();
+            if(b.equals("coupon end")){     // if coupon is end then it remove that is real use in company
+                i.remove();
+            }
+            
+        }
+        System.out.println(it);
+        
+    }
+}
