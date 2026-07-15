27a4ea38c961a87d6273d3ca6cb60c319572813f
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-15 12:13:10 +0530

    Thread Code

--- Collection/Generics.java
+++ Collection/Generics.java
@@ -1,12 +1,5 @@
-import java.util.ArrayList;
-import java.util.*;
-
 class Generics{
-    public static void main(String[] args) {
-        Collection <String> s = new ArrayList<>();
-        s.add("Nagesh");
-        s.add("Ramesh");
-        System.out.println(s);
-        //s.add(100);   // error occur due to Generics
+    public static void main(String[]args){
+
     }
 }
