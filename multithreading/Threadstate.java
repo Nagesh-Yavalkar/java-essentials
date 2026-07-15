9db3fbd23a216002656f003d1a91aef7ed110948
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-15 12:15:55 +0530

    Update Threadstate

--- multithreading/Threadstate.java
+++ multithreading/Threadstate.java
@@ -1,21 +1,21 @@
-class Task implements Runnable{
-    public void run(){
-        System.out.println(Thread.currentThread().getName());
-    }
-    
-}
-class Threadstate{
-    public static void main(String[] args) throws InterruptedException {
-        Task obj = new Task();
-        
-        Thread t1 = new Thread(obj);
-        System.out.println(Thread.currentThread().getState());
-       
-        Thread.sleep(2000);
-        
-        t1.start();
-        ;
-        System.out.println(Thread.currentThread().getName());
 
-    }
+class Task implements Runnable {
+  public void run(){
+    System.out.println(Thread.currentThread().getName());
+
+  }
+}
+
+class Threadstate{
+   public static void main(String[] var0) throws InterruptedException {
+      Task var1 = new Task();
+      System.out.println(Thread.currentThread().getState());
+      Thread var2 = new Thread(var1);
+      System.out.println(Thread.currentThread().getState());
+      Thread.sleep(2000L);
+      System.out.println(Thread.currentThread().getState());
+      var2.start();
+      System.out.println(Thread.currentThread().getState());
+      System.out.println(Thread.currentThread().getName());
+   }
 }
